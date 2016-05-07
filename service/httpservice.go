f34417ca11a0e05eed1207e26441f71ebd8e0e16
Rick Morrison <rmorriso@fedroot.com>
2016-05-07 11:44:54 -0700

    update copyright years

--- service/httpservice.go
+++ service/httpservice.go
@@ -1,4 +1,4 @@
-// Copyright 2015 The Metalogic Software Authors. All rights reserved.
+// Copyright 2012-2016 The Metalogic Software Authors. All rights reserved.
 // Use of this source code is governed by an MIT
 // license that can be found in the LICENSE file
 
